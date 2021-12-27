97429f9263ae45134a8c43c31f5ac1ef80a0410f
Vega-56 <84074060+Vega-56@users.noreply.github.com>
2021-12-28 01:01:44 +1100

    Fix bugs and finalise for build

--- components/ColorDetails.js
+++ components/ColorDetails.js
@@ -6,7 +6,7 @@ export default function ColorDetails({ route }) {
 	const color = Color(colorname);
 	const textColor = { fontSize: 30, color: color.negate().toString() };
 	return (
-		<View style={[styles.container, { backgroundColor: color }]}>
+		<View style={[styles.container, { backgroundColor: colorname }]}>
 			<Text style={textColor}>{colorname}</Text>
 			<Text style={textColor}>{color.rgb().toString()}</Text>
 			<Text style={textColor}>{color.hsl().toString()}</Text>
